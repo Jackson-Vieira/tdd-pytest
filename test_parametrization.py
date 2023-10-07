846620ce55fbcf215f774bd76a4dc0e921cb68b5
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-07 14:13:30 -0300

    parametrization version 2

--- test_parametrization.py
+++ test_parametrization.py
@@ -1,24 +1,43 @@
+import pytest
+
 def is_palindrome(s):
     s_without_spaces = s.replace(" ", "")
     return s_without_spaces.lower() == s_without_spaces[::-1].lower()
 
-def test_is_palindrome_empty_string():
-    assert is_palindrome("")
+# def test_is_palindrome_empty_string():
+#     assert is_palindrome("")
+#
+# def test_is_palindrome_single_character():
+#     assert is_palindrome("a")
+#
+# def test_is_palindrome_mixed_casing():
+#     assert is_palindrome("Bob")
+#
+# def test_is_palindrome_with_spaces():
+#     assert is_palindrome("Never odd or even")
+#
+# def test_is_palindrome_with_punctuation():
+#     assert is_palindrome("Do geese see God")
+#
+# def test_is_palindrome_not_palindrome():
+#     assert not is_palindrome("abc")
+#
+# def test_is_palindrome_not_quite():
+#     assert not is_palindrome("abab")
 
-def test_is_palindrome_single_character():
-    assert is_palindrome("a")
+@pytest.mark.parametrize("palindrome", [
+    "",
+    "a",
+    "Bob",
+    "Never odd or even",
+    "Do geese see God",
+])
+def test_is_palindrome(palindrome):
+    assert is_palindrome(palindrome)
 
-def test_is_palindrome_mixed_casing():
-    assert is_palindrome("Bob")
-
-def test_is_palindrome_with_spaces():
-    assert is_palindrome("Never odd or even")
-
-def test_is_palindrome_with_punctuation():
-    assert is_palindrome("Do geese see God")
-
-def test_is_palindrome_not_palindrome():
-    assert not is_palindrome("abc")
-
-def test_is_palindrome_not_quite():
-    assert not is_palindrome("abab")
+@pytest.mark.parametrize("non_palindrome", [
+    "abc",
+    "abab",
+])
+def test_is_palindrome_not_palindrome(non_palindrome):
+    assert not is_palindrome(non_palindrome)
